012fc324132564b166ae1454d78196bb148320b0
Alston Haynes <alston13@icloud.com>
2021-07-21 00:09:11 -0700

    IA

---
+++ java.js
@@ -0,0 +1,31 @@
+alert("Lets play 'What is my name'");
+
+var x;
+let nameEntered = prompt("Is my name Alston? Yes or No?");
+alert("yep that's my name.");
+
+let namenotEntered = prompt("Ok, then what is my name?");
+
+let nameSeriously = prompt("For real.");
+
+let nameUnknown =  prompt ("So this is what my name is?");
+
+
+//alert("I like that form too!")
+//function askshenron() {
+    //var shenron = false;
+    //while (shenron == false) {
+      //  shenron = prompt("How many Dragon Balls do you need to make a wish?");
+        //if (shenron < 7) {
+            //alert("Incorrect, try again.");
+            //shenron = false;
+
+
+        //}
+        //else {
+            //alert("Correct, you need 7 Dragon Balls!");
+
+       // }
+    //}
+//}
+//askshenron();
